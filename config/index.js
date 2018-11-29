3aa2785a286d57474d1b7d45cacfb8d52f473164
Eden Liu <eden90267@gmail.com>
2018-11-29 10:52:45 +0800

    Add dist folder

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue_vuex__exercise/dist',
 
     /**
      * Source Maps
