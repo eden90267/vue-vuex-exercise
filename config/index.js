169f8e8b004ecdf597aa524d46bace1a4f093fdc
eden90267 <eden90267@gmail.com>
2018-11-29 22:04:21 +0800

    Rebuild dist

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue_vuex__exercise/dist',
+    assetsPublicPath: '/vue-vuex-exercise/dist',
 
     /**
      * Source Maps
